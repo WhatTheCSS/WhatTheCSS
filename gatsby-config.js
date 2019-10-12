60ef396126fa983e35de26c52dc899c65c983f9a
Nathaniel Collins <nathancleon@gmail.com>
2019-10-12 12:53:34 -0400

    update fav icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `What the CSS`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `A 30 day email based course teaching CSS fundamentals via fun daily challenges and exercises.`,
     author: `@musicalwebdev`,
   },
   plugins: [
@@ -26,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/wtcss_logo_teal.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
